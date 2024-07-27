6924cabca1244e2a979a7add4305c3455f0705c1
PetarHr99 <122648207+PetarHr99@users.noreply.github.com>
2024-07-27 16:14:20 +0300

    User session fixed

--- src/main/java/bg/softuni/finalproject/web/HomeController.java
+++ src/main/java/bg/softuni/finalproject/web/HomeController.java
@@ -1,25 +1,38 @@
 package bg.softuni.finalproject.web;
 
+import bg.softuni.finalproject.config.UserSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final UserSession userSession;
+
+    public HomeController(UserSession userSession) {
+        this.userSession = userSession;
+    }
 
     @GetMapping("/")
     public String nonLoggedInIndex(){
-
+        if (userSession.isLoggedIn()){
+            return "redirect:/home";
+        }
         return "index";
     }
 
     @GetMapping("/features")
     public String showFeaturesPage() {
+        if (userSession.isLoggedIn()){
+            return "redirect:/home";
+        }
         return "features";
     }
 
     @GetMapping("/pricing")
     public String showPricingPage(){
+        if (userSession.isLoggedIn()){
+            return "redirect:/home";
+        }
         return "pricing";
     }
 
